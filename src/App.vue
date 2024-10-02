c8d84df95886ec029e7d3485c7dd146e59c1cd88
IanFan <fancyian05180910@gmail.com>
2024-10-02 19:03:41 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <HeaderComponent
+      back-control
+      scan-control
+      wallet-control
+      logo-type
+      :title-text="'我是 Title Text'"
+      @backEvent="backEvent"
+      @scanEvent="scanEvent"
+      @walletEvent="walletEvent"
+    />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import HeaderComponent from './components/HeaderComponent.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const backEvent = () => {
+  console.log('元件外部執行 backEvent !')
 }
+
+const scanEvent = () => {
+  console.log('元件外部執行 scanEvent !')
+}
+
+const walletEvent = () => {
+  console.log('元件外部執行 walletEvent !')
+}
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  width: 760px;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: #fff;
 }
 </style>
+
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="header">
+    <div class="left_icon">
+      <img
+        v-if="backControl"
+        class="icon"
+        src="../assets/back.png"
+        width="20"
+        @click="backEvent"
+      />
+    </div>
+    <div>
+      <div v-if="props.logoType">
+        <img class="logo" src="../assets/openwallet.png">
+      </div>
+      <div v-if="props.titleType">{{ props.titleText }}</div>
+    </div>
+    <div class="right_icon">
+      <img
+        v-if="scanControl"
+        class="icon"
+        :class="{'mr-10': walletControl}"
+        src="../assets/scanner.png"
+        @click="scanEvent"
+      >
+      <img
+        v-if="walletControl"
+        class="icon"
+        src="../assets/wallet.png"
+        @click="walletEvent"
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  logoType: {
+    type: Boolean,
+    default: false
+  },
+  titleType: {
+    type: Boolean,
+    default: false
+  },
+  titleText: {
+    type: String,
+    default: ''
+  },
+  backControl: {
+    type: Boolean,
+    default: false
+  },
+  scanControl: {
+    type: Boolean,
+    default: false
+  },
+  walletControl: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emit = defineEmits(['backEvent', 'scanEvent', 'walletEvent'])
+
+const backEvent = () => {
+  emit('backEvent')
+}
+
+const scanEvent = () => {
+  emit('scanEvent')
+}
+
+const walletEvent = () => {
+  emit('walletEvent')
+}
+
+</script>
+
+<style scoped>
+.header {
+  padding: 10px 20px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.left_icon {
+  width: 20px;
+}
+.right_icon {
+  display: flex;
+}
+.logo {
+  display: block;
+  width: 180px;
+}
+.icon {
+  display: block;
+  width: 20px;
+  cursor: pointer;
+}
+.mr-10{
+  margin-right: 10px;
+}
+</style>
